7297cb03466b2cbdc9b50cd684a5f1d8bc06f697
AndrewVarshave <vad_15_05@bk.ru>
2025-08-09 11:17:03 +0300

    добавлены прототипы функций и сделаны циклы дря работы с ними

--- main.c
+++ main.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
+void menu(void){
+    printf("menu\n");
+}
+void add_vector(void){
+    printf("1\n");
+}
+void view_vectors(void){
+    printf("2\n");
+}
+void delete_vector(void){
+    printf("3\n");
+}
+void sum_vectors(void){
+    printf("4\n");
+}
+void scalarnoe(void){
+    printf("5\n");
+}
+void clean(void){
+    printf("6\n");
+}
  int main(){
-    printf("1");
+    int action;
+    int run = 1;
+    menu();
+    while (run == 1) {
+        printf("\n> Select action: ");
+        scanf("%d", &action);
+        switch (action) {
+            case 1: add_vector(); break;
+            case 2: view_vectors(); break;
+            case 3: delete_vector(); break;
+            case 4: sum_vectors(); break;
+            case 5: scalarnoe(); break;
+            case 6: clean(); run=0; break;
+            default : printf("\nWrong action"); break;
+        }
+    }
     return 0;
  }
